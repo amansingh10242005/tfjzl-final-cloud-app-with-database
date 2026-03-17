78d254705901d96c35ede761a696d126a88fa6a8
Aman Singh <amandeaman12345@gmail.com>
2026-03-17 13:47:04 +0530

    Final Django Online Course Project

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,43 +1,31 @@
 from django.contrib import admin
 
-# Import models
-
 from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
-# Inline classes
-
 class LessonInline(admin.StackedInline):
-model = Lesson
-extra = 5
+    model = Lesson
+    extra = 5
 
 class ChoiceInline(admin.StackedInline):
-model = Choice
-extra = 2
+    model = Choice
+    extra = 2
 
 class QuestionInline(admin.StackedInline):
-model = Question
-extra = 2
-
-# Course admin configuration
+    model = Question
+    extra = 2
 
 class CourseAdmin(admin.ModelAdmin):
-inlines = [LessonInline]
-list_display = ('name', 'pub_date')
-list_filter = ['pub_date']
-search_fields = ['name', 'description']
-
-# Question admin configuration
+    inlines = [LessonInline]
+    list_display = ('name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
 
 class QuestionAdmin(admin.ModelAdmin):
-inlines = [ChoiceInline]
-list_display = ['content']
-
-# Lesson admin configuration
+    inlines = [ChoiceInline]
+    list_display = ['content']
 
 class LessonAdmin(admin.ModelAdmin):
-list_display = ['title']
-
-# Register models
+    list_display = ['title']
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
